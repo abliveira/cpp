2e1e74c073b0119b250d57e0369e40031ec25db1
abliveira <41597910+abliveira@users.noreply.github.com>
2020-05-19 10:31:02 -0300

    Create Dynamic memory.cpp

---
+++ Compound data types/Dynamic memory.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+/*  Na linguagem C++, a memória é dividida em duas partes:
+        Stack: Espaço de memória de programa definido no momento da compilação
+        Heap: Espaço de memória de programa não utilizado que é usadado para alocar variáveis dinamicamente
+            na memória durante o tempo de execução do programa
+    Quando não se sabe ao certo quanto de memória será usado durante a execução do programa, como a dependência
+        de entrada pelo usuário, pode-se reservar dinamicamente a memória */
+
+int main () {
+
+    // A alocação de memória é feita com ponteiros. O ponteiro é armazenado na memória stack
+    // Como ainda não há um valor, pode-se inicializa-lo com NULL
+    double* p  = NULL;
+    
+    cout << "Endereço apontado por p: " << p << endl;
+    
+    // O operador new é usado para requisitar memória referente a um tipo de dado
+    p  = new double;
+    
+    // Pode-se então atribuir um valor a memória reservada utilizando o ponteiro
+    *p = 32000;
+    cout << "Endereço apontado por p: " << p << endl;
+    cout << "Valor apontado por p: " << *p << endl;
+    
+    // Quando não for mais necessária, a memória alocada pode ser liberada com o operador delete
+    delete p;
+    
+    // O ponteiro no entanto, ainda guarda o valor até ser novamente usado
+    cout << "Endereço apontado por p: " << p << endl;
+    cout << "Valor apontado por p: " << *p << endl;
+    
+    // A memória também pode ser alocada para arrays
+    int *a = NULL;
+    a = new int[20];
+    delete [] a;
+    
+    return 0;
+}
