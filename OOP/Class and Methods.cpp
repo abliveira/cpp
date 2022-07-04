7e687d286e69b948229bdd688c5c08a9d5460e39
abliveira <dev.ab@outlook.com>
2022-07-03 23:19:45 -0300

    Update Class and Methods.cpp

--- OOP/Class and Methods.cpp
+++ OOP/Class and Methods.cpp
@@ -7,6 +7,16 @@ which can be accessed and used by creating an instance of that class
 
 An Object is an instance of a Class. When a class is defined, no memory is allocated 
 but when it is instantiated (i.e. an object is created) memory is allocated.
+
+
+
+When working with an object, use the dot member selection operator (.).
+When working with a pointer to the object, use the arrow member selection operator (->).
+
+Example:
+MyClass obj;
+    MyClass *ptr = &obj;
+    ptr->myPrint();
 */
 
 #include <iostream>
